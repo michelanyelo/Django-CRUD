c8b51a7e8cfbe8830fbef630258468bcc4f1e6c4
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2023-12-30 21:30:34 -0300

    Panel de administración

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+# importar modelos para visualizarlos en internet
+from .models import Categoria, Producto
 # Register your models here.
+
+# con lo anterior añadir estas instrucciones
+admin.site.register(Categoria)
+admin.site.register(Producto)
+
