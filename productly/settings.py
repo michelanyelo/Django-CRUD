fbd013f657b594ffafe9b36f7576900c365aff6b
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2023-12-31 13:59:42 -0300

    Personalizando formulario (github/django + bootstrap classes)

--- productly/settings.py
+++ productly/settings.py
@@ -1,4 +1,3 @@
-import os
 """
 Django settings for productly project.
 
@@ -10,8 +9,14 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from django.forms.renderers import TemplatesSetting
+
+class CustomFormRenderer(TemplatesSetting):
+    form_template_name = "form_snippet.html"
+
+FORM_RENDERER = "productly.settings.CustomFormRenderer"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
