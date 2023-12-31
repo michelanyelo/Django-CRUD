292d1b9cd22911ebd27d2a49212bae49fdb364a3
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2023-12-31 12:59:22 -0300

    Obtener un elemento

--- productos/views.py
+++ productos/views.py
@@ -17,5 +17,7 @@ def index(request):
         context={"productos": productos},
     )
 
+
 def detalle(request, producto_id):
-    return HttpResponse(producto_id)
+    producto = Producto.objects.get(id=producto_id)
+    return render(request, "detalle.html", context={"producto": producto})
