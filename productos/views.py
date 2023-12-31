e98e20532d9a3c634da2995067f95a6d3270003b
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2023-12-31 13:06:59 -0300

    Manejo de Error 404

--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 # Create your views here.
@@ -19,5 +19,5 @@ def index(request):
 
 
 def detalle(request, producto_id):
-    producto = Producto.objects.get(id=producto_id)
+    producto = get_object_or_404(Producto, id=producto_id)
     return render(request, "detalle.html", context={"producto": producto})
