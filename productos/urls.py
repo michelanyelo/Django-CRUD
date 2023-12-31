dbb1e97108c4ac9a6c40a42e2a49e5ab2b18865c
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2023-12-31 13:11:51 -0300

    Manejo de links con variable APP

--- productos/urls.py
+++ productos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "productos"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:producto_id>", views.detalle, name="producto_detalle"),
+    path("", views.index, name="ndex"),
+    path("<int:producto_id>", views.detalle, name="detalle"),
 ]
